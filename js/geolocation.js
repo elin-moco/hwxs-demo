dfa6a402e0662f03716e6b3966f38969cfb2df6f
Eddie Lin <elin@mozilla.com>
2015-05-24 03:20:59 +0800

    Implemented geolocation and screen orientation demos.

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,20 +1,71 @@
 (function() {
   'use strict';
 
+  function loadScript(filename, filetype, callback) {
+    var fileref = null;
+    if (filetype == "js") { //if filename is a external JavaScript file
+      fileref = document.createElement('script');
+      fileref.setAttribute("type", "text/javascript");
+      fileref.setAttribute("src", filename)
+    }
+    else if (filetype == "css") { //if filename is an external CSS file
+      fileref = document.createElement("link");
+      fileref.setAttribute("rel", "stylesheet");
+      fileref.setAttribute("type", "text/css");
+      fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref != "undefined") {
+      fileref.onload = callback;
+      document.getElementsByTagName("head")[0].appendChild(fileref)
+    }
+  }
+
   var Geolocation;
   Geolocation = {
-    init: function() {
+    marker: null,
+    options: null,
+    map: null,
+    geoHandler: function() {
       Geolocation.watchId = navigator.geolocation.watchPosition(
         function(position) {
           document.getElementById('latitude').textContent =
             position.coords.latitude;
           document.getElementById('longitude').textContent =
             position.coords.longitude;
-          document.getElementById('altitude').textContent =
-            position.coords.altitude;
-          document.getElementById('accuracy').textContent =
-            position.coords.accuracy;
+          var latLong = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+          Geolocation.options = {
+            zoom: 15,
+            center: latLong
+          };
+
+          if (Geolocation.map) {
+            Geolocation.map.set(Geolocation.options);
+          }
+          else {
+            Geolocation.map = new google.maps.Map(document.getElementById('map'),
+              Geolocation.options);
+          }
+
+          if (Geolocation.marker) {
+            Geolocation.marker.setMap(null);
+          }
+          Geolocation.marker = new google.maps.Marker({
+            position: latLong,
+            map: Geolocation.map,
+            title: 'Hello World!'
+          });
         });
+    },
+    init: function() {
+      window.initMap = Geolocation.geoHandler;
+      if (Geolocation.map == null) {
+        loadScript('https://maps.googleapis.com/maps/api/js?v=3.exp' +
+          '&signed_in=true&callback=initMap', 'js');
+      }
+      else {
+        Geolocation.geoHandler();
+      }
     },
     destroy: function() {
       navigator.geolocation.clearWatch(Geolocation.watchId);
--- js/screen-orientation.js
+++ js/screen-orientation.js
@@ -3,8 +3,16 @@
 
   var ScreenOrientation;
   ScreenOrientation = {
+    angles: {
+      'portrait-primary': '0deg',
+      'landscape-primary': '-90deg',
+      'portrait-secondary': '180deg',
+      'landscape-secondary': '90deg'
+    },
     handler: function() {
       document.getElementById('screen').textContent = screen.mozOrientation;
+      document.getElementById('smartphone').style.transform =
+        'rotate('+ScreenOrientation.angles[screen.mozOrientation]+')';
     },
     init: function() {
       ScreenOrientation.handler();
